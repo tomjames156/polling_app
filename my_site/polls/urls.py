a75f6bf36e47e971b4958905cc2022a286216215
Akinwande Tomisin <tomjames156@gmail.com>
2023-02-11 08:43:09 +0100

    added detail, vote and results views

--- my_site/polls/urls.py
+++ my_site/polls/urls.py
@@ -1,7 +1,11 @@
 from django.urls import path
 
-from .views import index
+from .views import index, detail, results, vote
 
 urlpatterns = [
-    path('', index, name='index')
+    path('', index, name='index'), # index
+    path('<int: question_id>/', detail, name='detail'), # detail
+    path('<int: question_id>/results/', results, name='results'),
+    path('<int: question_id>/vote/', vote, name='vote')
+
 ]
--- my_site/polls/views.py
+++ my_site/polls/views.py
@@ -4,3 +4,12 @@ from django.shortcuts import render, HttpResponse
 
 def index(request):
     return HttpResponse("Hello World, You're at the polls index")
+
+def detail(request, question_id):
+    return HttpResponse(f"You are looking at Question {question_id}")
+
+def results(request, question_id):
+    return HttpResponse(f"You are looking at the results of Question {question_id}")
+
+def vote(request, question_id):
+    return HttpResponse(f"You are currently voting for Question {question_id}")
