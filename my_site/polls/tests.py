616784c92fca8fcd252875e29c2ad3237e905899
Akinwande Tomisin <tomjames156@gmail.com>
2023-02-12 18:00:23 +0100

    added a test to filter future dates

--- my_site/polls/tests.py
+++ my_site/polls/tests.py
@@ -1,3 +1,16 @@
+import datetime
+
+
+from django.utils import timezone
 from django.test import TestCase
+from polls.models import Question
 
 # Create your tests here.
+
+class QuestionModelTests(TestCase):
+
+    def test_was_published_recently_with_future_date(self): 
+        """Returns False if a question was published recently with a future date"""
+    time = timezone.now() + datetime.timedelta(days=30)
+    future_question  = Question(pub_date=time)
+    assert(future_question.was_published_recently(), False)
