6ee7b29a89e4ef4e029df6d5cd346139fef9f18d
Akinwande Tomisin <tomjames156@gmail.com>
2023-02-12 15:13:43 +0100

    changes index and detail views to generic views

--- my_site/polls/urls.py
+++ my_site/polls/urls.py
@@ -1,12 +1,12 @@
 from django.urls import path
 
-from .views import index, detail, results, vote
+from . import views
 
 app_name = 'polls'
 urlpatterns = [
-    path('', index, name='index'), # index
-    path('<int:question_id>/', detail, name='detail'), # detail
-    path('<int:question_id>/results/', results, name='results'),
-    path('<int:question_id>/vote/', vote, name='vote')
+    path('', views.IndexView.as_view(), name='index'), # index
+    path('<int:pk>/', views.DetailView.as_view(), name='detail'), # detail
+    path('<int:pk>/results/', views.results, name='results'),
+    path('<int:question_id>/vote/', views.vote, name='vote')
 
 ]
--- my_site/polls/views.py
+++ my_site/polls/views.py
@@ -1,36 +1,22 @@
 from django.shortcuts import HttpResponse, render, get_object_or_404, HttpResponseRedirect
 from django.urls import reverse
+from django.db.models import F
+from django.views import generic
 from .models import Question, Choice
 
 # Create your views here.
 
-def index(request):
-    latest_question_list = Question.objects.order_by('pub_date')[:5]
-    context = {
-        'latest_question_list': latest_question_list
-    }
-    return render(request, 'polls/index.html', context)
+class IndexView(generic.ListView):
+    template_name = 'polls/index.html'
+    context_object_name = 'latest_question_list'
 
-def detail(request, question_id):
-    poll_question = get_object_or_404(Question, id=question_id)
-    choices = poll_question.choice_set.all()
-    context = {
-        'question': poll_question,
-        'choices': choices
-    }
-    return render(request, 'polls/detail.html', context)
-
-def results(request, question_id):
-    question = get_object_or_404(Question, pk=question_id)
-    choices = question.choice_set.all()
-    total = sum([choice.votes for choice in choices])
-    context = {
-        'question': question, 
-        'choices': choices,
-        'total': total,
-        }
+    def get_queryset(self):
+        # Returns the last 5 posted polls
+        return Question.objects.order_by('pub_date')[:4]
     
-    return render(request, 'polls/results.html', context)
+class DetailView(generic.DetailView):
+    model = Question
+    template_name = 'polls/detail.html'
 
 def vote(request, question_id):
     question = get_object_or_404(Question, pk=question_id)
@@ -42,7 +28,18 @@ def vote(request, question_id):
             'error_message': 'You did not select a choice'
         })
     else:
-        selected_choice.votes += 1
+        selected_choice.votes = F('votes') + 1
         selected_choice.save()
 
         return HttpResponseRedirect(reverse('polls:results', args=(question.id,)))
+
+def results(request, pk):
+    question = Question.objects.get(pk=pk)
+    choices = question.choice_set.all()
+    total = sum(choice.votes for choice in choices)
+    context = {
+        'question': question,
+        'choices': choices, 
+        'total': total
+    }
+    return render(request, 'polls/results.html', context)
