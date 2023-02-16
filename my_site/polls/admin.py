c52dead7ff97cb7cb996c5ecd98d6bd658288c06
Akinwande Tomisin <tomjames156@gmail.com>
2023-02-16 09:18:45 +0100

    added admin base template

--- my_site/polls/admin.py
+++ my_site/polls/admin.py
@@ -10,6 +10,9 @@ class QuestionAdminModel(admin.ModelAdmin):
     fieldsets = [
         ('Question information', {'fields': ['question_text', 'pub_date'], 'classes': ['collapse']}),
     ]
+    list_filter = ['pub_date']
+    search_fields = ['question_text', 'choice__choice_text']
+    list_display = ('question_text', 'pub_date', 'was_published_recently')
     inlines = [ChoiceInline]
 
 admin.site.register(Question, QuestionAdminModel)
--- my_site/polls/models.py
+++ my_site/polls/models.py
@@ -1,6 +1,7 @@
 import datetime 
 from django.db import models
 from django.utils import timezone
+from django.contrib import admin
 
 # Create your models here.
 
@@ -10,6 +11,12 @@ class Question(models.Model):
 
     def __str__(self):
         return self.question_text
+
+    @admin.display(
+        boolean = True,
+        ordering = 'pub_date',
+        description = 'Published Recently?'
+    )
 
     def was_published_recently(self):
         yesterday = timezone.now() - datetime.timedelta(days=1)
