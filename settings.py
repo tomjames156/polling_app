77a77667ea0d011a7b059e8ac3a3c2149daa8ec9
Akinwande Tomisin <tomjames156@gmail.com>
2023-02-16 14:36:19 +0100

    moved files out of the my_site directory

--- settings.py
+++ settings.py
@@ -25,11 +25,7 @@ SECRET_KEY = 'django-insecure-r3q%2#@dqu816xkz76r43v32&g=p9m@n54n^$ghmn08cd@ag#+
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = [
-  'localhost',
-  '127.0.0.1',
-  '111.222.333.444',
-  'mywebsite.example', '.vercel.app']
+ALLOWED_HOSTS = ['*']
 
 # Application definition
 
